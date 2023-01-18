9949ac3dc30149e23d34f60494a2f64381749ef5
Rich Newman <rich-newman@noreply.github.com>
2023-01-18 17:14:23 +0000

    Initial commit

---
+++ ImageMagicPotrace/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+#nullable disable 
+namespace ImageMagicPotrace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetAssembly(typeof(Program)).Location);
+            string filesPath = Path.Combine(path, "Files");
+
+            string fileName = "image";
+
+            string imageMagick = "magick.exe";
+            string arg = $"convert \"{Path.Combine(filesPath, fileName + ".png")}\" \"{Path.Combine(filesPath, fileName + ".pnm")}\"";
+            RunProcess(imageMagick, arg);
+
+            string potrace = Path.Combine(path, @"potrace\potrace.exe");
+            string arg2 = $"\"{Path.Combine(filesPath, fileName + ".pnm")}\" -b svg";
+            RunProcess(potrace, arg2);
+
+            Console.WriteLine("Done");
+        }
+
+        private static void RunProcess(string executable, string arg)
+        {
+            Console.WriteLine(executable);
+            Console.WriteLine(arg);
+            ProcessStartInfo start = new ProcessStartInfo(executable)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = arg,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process process = Process.Start(start);
+            process.WaitForExit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ImageMagickPotrace
+Solution to a [Stack Overflow question](https://stackoverflow.com/questions/75096682/execute-multiple-command-lines-with-the-different-process-using-net).
+
+Needs [ImageMagick installed to work](https://imagemagick.org/script/download.php).  ImageMagick must be installed before starting VS and must have ImageMagick put on the path, which it is by default.
+
+Converts the file {fileName}.png in ImageMagicPotrace\ImageMagicPotrace\Files to a smoothed svg file using ImageMagick and potrace, with the result in ImageMagicPotrace\ImageMagicPotrace\bin\Debug\net6.0\Files.
